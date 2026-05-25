b32073ea24cca593e3ac034d6e76b3db08c9644f
khiemphamdev <khiempham.dev@gmail.com>
2026-05-26 04:58:01 +0700

    Thêm CRUD khách hàng

--- BookStore_65131483/BookStore_65131483/Controllers/TrangAdminController.cs
+++ BookStore_65131483/BookStore_65131483/Controllers/TrangAdminController.cs
@@ -42,5 +42,60 @@ namespace BookStore_65131483.Controllers
             return View();
         }
 
+        // ================== QUẢN LÝ USER ==================
+        public ActionResult UserList()
+        {
+            var list = db.TAIKHOANs
+                         .OrderBy(t => t.MaTK)
+                         .Skip(1)
+                         .ToList();
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult EditUser(int id)
+        {
+            var tk = db.TAIKHOANs.Find(id);
+            return View(tk);
+        }
+
+        [HttpPost]
+        public ActionResult EditUser(TAIKHOAN tk)
+        {
+            var old = db.TAIKHOANs.Find(tk.MaTK);
+            if (db.TAIKHOANs.Any(x => x.Email == tk.Email && x.MaTK != tk.MaTK))
+            {
+                ViewBag.Error = "Email đã tồn tại";
+                return View(tk);
+            }
+
+            if (db.TAIKHOANs.Any(x => x.SDT == tk.SDT && x.MaTK != tk.MaTK))
+            {
+                ViewBag.Error = "Số điện thoại đã tồn tại";
+                return View(tk);
+            }
+
+            old.HoTen = tk.HoTen;
+            old.Email = tk.Email;
+            old.SDT = tk.SDT;
+            old.DiaChi = tk.DiaChi;
+            old.VaiTro = tk.VaiTro;
+
+            db.SaveChanges();
+            return RedirectToAction("UserList");
+        }
+
+        public ActionResult DeleteUser(int id)
+        {
+            var tk = db.TAIKHOANs.Find(id);
+            if (tk != null)
+            {
+                db.TAIKHOANs.Remove(tk);
+                db.SaveChanges();
+            }
+            return RedirectToAction("UserList");
+        }
+
     }
 }
