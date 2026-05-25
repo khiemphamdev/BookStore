1931cf612ecbafb9f06f8b54fb302fde9b0f6847
khiemphamdev <khiempham.dev@gmail.com>
2026-05-26 06:54:01 +0700

    Thêm chức năng quản lý tài khoản và đơn hàng

--- BookStore_65131483/BookStore_65131483/Controllers/TaiKhoanController.cs
+++ BookStore_65131483/BookStore_65131483/Controllers/TaiKhoanController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Security;
 using BookStore_65131483.Models;
@@ -97,6 +98,85 @@ namespace BookStore_65131483.Controllers
         {
             return FormsAuthentication.HashPasswordForStoringInConfigFile(password, "SHA1");
         }
+
+        public ActionResult TaiKhoan()
+        {
+            var maTK = GetMaTK();
+            if (maTK == null)
+                return RedirectToAction("Login", "TaiKhoan_65131483");
+
+            var tk = db.TAIKHOANs.Find(maTK);
+            return View(tk);
+        }
+        [HttpPost]
+        public ActionResult TaiKhoan(TAIKHOAN model, HttpPostedFileBase Avatar)
+        {
+            var maTK = GetMaTK();
+            if (maTK == null)
+                return RedirectToAction("Login", "TaiKhoan_65131483");
+
+            var tk = db.TAIKHOANs.Find(maTK);
+
+            if (model.HoTen != null)
+                tk.HoTen = model.HoTen;
+            if (model.Email != null)
+                tk.Email = model.Email;
+            if (model.SDT != null)
+                tk.SDT = model.SDT;
+            if (model.DiaChi != null)
+                tk.DiaChi = model.DiaChi;
+
+            // Upload avatar
+            //if (Avatar != null && Avatar.ContentLength > 0)
+            //{
+            //    string fileName = System.IO.Path.GetFileName(Avatar.FileName);
+            //    string path = Server.MapPath("~/Content/Avatar/" + fileName);
+            //    Avatar.SaveAs(path);
+            //    tk. = fileName;
+            //}
+
+            db.SaveChanges();
+            ViewBag.Success = "Cập nhật thành công";
+            return View(tk);
+        }
+
+
+        // Lấy mã tài khoản đang đăng nhập
+        private int? GetMaTK()
+        {
+            return Session["MaTK"] as int?;
+        }
+        public ActionResult DonHang()
+        {
+            var maTK = GetMaTK();
+            if (maTK == null)
+                return RedirectToAction("Login", "TaiKhoan_65131483");
+
+            var donHangs = db.DONHANGs
+                .Include(d => d.CHITIETDONHANGs)
+                .Where(d => d.MaTK == maTK && d.TrangThai != "TrongGioHang" && d.TrangThai != "Đã hủy")
+                .OrderByDescending(d => d.NgayDat)
+                .ToList();
+
+            return View(donHangs);
+        }
+        public ActionResult ChiTietDonHang(int maDH)
+        {
+            var ctdh = db.CHITIETDONHANGs
+                .Include(d => d.SACH)
+                .Where(d => d.MaDH == maDH)
+                .ToList();
+
+            return View(ctdh);
+        }
+        public ActionResult HuyDon(int maDH)
+        {
+            var donHang = db.DONHANGs.Find(maDH);
+            db.DONHANGs.Remove(donHang);
+            db.SaveChanges();
+            return RedirectToAction("DonHang", "TaiKhoan_65131483");
+        }
+        
 
     }
 }
