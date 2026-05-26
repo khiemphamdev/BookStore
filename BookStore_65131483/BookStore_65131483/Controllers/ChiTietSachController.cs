a4a3e4a1d68b167490bb3416a420eb240464ddab
khiemphamdev <khiempham.dev@gmail.com>
2026-05-26 09:49:51 +0700

    Thêm chức năng chi tiết sách

---
+++ BookStore_65131483/BookStore_65131483/Controllers/ChiTietSachController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore_65131483.Models;
+using System.Data.Entity;
+namespace BookStore_65131483.Controllers
+{
+    public class ChiTietSach_65131483Controller : Controller
+    {
+        // GET: ChiTietSach_65131483
+        BookStore_65131483Entities db = new BookStore_65131483Entities();
+
+
+        // GET: /Sach/Details/5
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = db.SACHes
+             .Include(s => s.DANHGIAs)      // load đánh giá
+             .FirstOrDefault(s => s.MaSach == id);
+            if (sach == null)
+                return HttpNotFound();
+
+            return View(sach);
+        }
+    }
+}
